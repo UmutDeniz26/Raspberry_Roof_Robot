db32333bf8695309b75f7e7167a23c49b8b48212
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-03-10 18:55:04 +0300

    Test

--- start_stream.sh
+++ start_stream.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rpicam-vid -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/stream1}' :demux=h264
+rpicam-vid -t 0 -n --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8080/stream1}' :demux=h264
