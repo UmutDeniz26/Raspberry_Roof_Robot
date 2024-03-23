fd05d23671efd92dbb56f905f15cd95dfa5ae160
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-03-23 11:53:22 +0300

    Refactor Arduino communication code

--- Tests/Communication_Arduino/Raspberry_side.py
+++ Tests/Communication_Arduino/Raspberry_side.py
@@ -2,14 +2,37 @@
 import serial
 import time
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # Change '/dev/ttyACM0' to '/dev/ttyUSB0' as per your lsusb output
-    ser.flushInput()  # Use flushInput instead of reset_input_buffer
+def transmit_receive_arduino_message( message, wait_delay = 0 ):
+
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # Open serial port
+    ser.flushInput()  # To clear the input buffer
     
-    ser.write(b"Hello from Raspberry Pi!\n")
-    line = ser.readline().decode('utf-8').rstrip()
-    if line:  # Check if line is not empty
-        print(line)
-    time.sleep(1)
+    #ser.write(b"{}\n".format(message).encode('utf-8'))  # Send message to Arduino
+    ser.write(message.encode('utf-8'))  
+
+    # Wait for the Arduino to process the message
+    if wait_delay > 0:
+        hold_time = time.time()
+        # if wait_delay is 1000 then it will wait for 1 second
+        while time.time() - hold_time < wait_delay:
+            line = ser.readline().decode('utf-8').rstrip()
+            if line:
+                print("Message taken while waiting.")
+                break
+    
+    if not line:
+        line = ser.readline().decode('utf-8').rstrip()
+        print("Message taken after waiting.")
+
+    if line:
+        recieved = line
+    else:
+        recieved = {"error": "No response from Arduino"}
     
     ser.close()
+
+    return recieved
+
+
+if __name__ == '__main__':
+    print("Use main function to send message to Arduino")
---
+++ Tests/Communication_Arduino/main.py
@@ -0,0 +1,3 @@
+import Raspberry_side
+
+Raspberry_side.transmit_receive_arduino_message("Hello Arduino")
