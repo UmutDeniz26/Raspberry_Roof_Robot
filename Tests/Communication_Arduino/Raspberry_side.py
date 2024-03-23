331f53bbff01a2b50baf4043ee9c5aff5d839af7
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-03-23 13:33:02 +0300

    Change file structurs

--- Tests/Communication_Arduino/Raspberry_side.py
+++ Tests/Communication_Arduino/Raspberry_side.py
@@ -33,10 +33,39 @@ def transmit_receive_arduino_message(message: str, timeout_limit: int):
         time.sleep(0.1)
 
 
+def json_dict_to_string(json_dict: dict) -> str:
+    """
+        Convert a dictionary to a json string. Deletes \n characters from the string. 
+        Then adds a newline character at the end.
+
+        Args:
+            json_dict (dict): The dictionary to convert to a json string.
+
+        Returns:
+            str: The json string.
+    """
+    json_string = str(json_dict).replace("\n", "").replace("\"", "'")
+    return json_string + "\n"
+
+
+def get_system_clock_time() -> str:
+    """
+        Get the current system clock time in seconds. In format of hh:mm:ss.
+
+        Returns:
+            int: The current system clock time in seconds.
+    """
+    return time.strftime("%H:%M:%S")
+
 # Test the function
 def test():
-    response = transmit_receive_arduino_message("Hello Arduino! \n", 10)
-    print("Response from Arduino:", response)
+    # Generate a sample message
+    json_message = {"message": "Hello Arduino how are you", "transmit_time": get_system_clock_time()}
+    string_message = json_dict_to_string(json_message)
+
+    # Transmit the message and receive the response
+    response = transmit_receive_arduino_message(string_message, 10)
+    print(response)
 
 if __name__ == "__main__":
     test()
