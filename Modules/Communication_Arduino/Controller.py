e4b08135a6c8683c33c1daf73ff7d8344a652c75
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-05-31 12:35:12 +0300

    Delete unnecessary codes

--- Modules/Communication_Arduino/Controller.py
+++
@@ -1,7 +0,0 @@
-import Raspberry.server_operations.Send_message as rasp
-
-json_dict = { "command": "forward", "speed": 100 , "transmit_time": rasp.get_system_clock_time() }
-    
-response = rasp.transmit_receive_arduino_message(json_dict, 5)
-
-print(response)
