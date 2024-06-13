78ff714f85a385082c0e2d98ae27e98ce9073f72
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-06-13 20:02:59 +0300

    chore: Add camera streaming functionality to Raspberry server

--- Raspberry/main/alternative.py
+++ Raspberry/main/alternative.py
@@ -5,6 +5,7 @@ import re
 import socket
 import traceback
 import sys
+import threading
 
 sys.path.insert(0,"Raspberry")
 sys.path.insert(0,".")
@@ -12,6 +13,7 @@ sys.path.insert(0,".")
 
 from Modules.Distance_Sensor.get_distance import get_max_distance
 from others.Stream_operations import get_host_ip_and_port
+from Modules.Camera.camera_stream import camera_stream_start
 
 class Common_Operations:
     def __init__(self):
@@ -226,24 +228,32 @@ class Client_Server_Operations(Common_Operations):
             return self.conn, self.addr
 
 class Raspberry_Server( Common_Operations ):
-    def __init__(self, HOST: str, PORT: int, dev: str, baud_rate: int):
+    def __init__(self, HOST: str, PORT: int, dev: str, baud_rate: int, camera_port: int):
         """
             Initialize the server.
 
             Args:
                 HOST (str): The IP address to connect to.
                 PORT (int): The port to connect to.
+                dev (str): The device name of the serial port.
+                baud_rate (int): The baud rate of the serial port.
+                camera_port (int): The port to start the camera stream on.
         """
         super().__init__()
         
-        # Composition of classes
+        # Parameters
         self.HOST = HOST; self.PORT = PORT; self.SERIAL_PORT_DEVICE = dev; self.SERIAL_PORT_BAUD_RATE = baud_rate
+        self.CAMERA_PORT = camera_port
+        
+        # Composition of classes
         self.server_operations = Client_Server_Operations(HOST, PORT)
         self.serial_port_operations = Serial_Port_Operations(dev, baud_rate)
 
         # Initialize components
         self.server_operations.init_server(HOST, PORT)
         self.serial_port_operations.init_serial_port(baud_rate, dev)
+        self.start_camera_stream_thread(self.CAMERA_PORT)
+
 
         self.print_features()
     
@@ -259,8 +269,23 @@ class Raspberry_Server( Common_Operations ):
         print(f"  Serving on {self.HOST}:{self.PORT}")
         print(f"  Serial port status:","Active" if hasattr(self.serial_port_operations, 'ser') else "Inactive")
         print(f"  Serial port on {self.SERIAL_PORT_DEVICE} with baud rate {self.SERIAL_PORT_BAUD_RATE}")
+        print(f"  Camera Status:","Serving on {self.CAMERA_PORT}" if hasattr(self, 'thread') else "Inactive")
         print()
 
+
+    def start_camera_stream_thread(self, port: int) -> None:
+        """
+        Starts the camera stream on a new thread.
+
+        Args:
+            port (int): The port to start the camera stream on.
+        """
+        try:
+            thread = threading.Thread(target=camera_stream_start, args=(port,))
+            thread.start()
+            time.sleep(0.5)
+        except Exception as e:
+            print(f"An error occurred while starting the camera stream: {e}")
 
     def run(self):
         """
@@ -273,17 +298,17 @@ class Raspberry_Server( Common_Operations ):
             try:
                 with self.server_operations.conn:
                     while True:
-                        # Start the distance measurement
-                        if  "time_hold" not in locals() or time.time() - time_hold > 1:
-                            max_distance = get_max_distance( 'Modules/Distance_Sensor/VL53L3CX_rasppi/vl53l3cx_ranging_output.txt' )
-                            print("Max distance is ", max_distance)
-                            time_hold = time.time()
-
+                        
                         # Receive the data from the client and check if it is available
                         received_data = self.server_operations.get_message_from_client()
                         if received_data is None:
                             continue
                         
+                        # Get the max distance
+                        max_distance = get_max_distance( 'Modules/Distance_Sensor/VL53L3CX_rasppi/vl53l3cx_ranging_output.txt' )
+                        print("Max distance is ", max_distance)
+                            
+
                         # Decode the data and convert to json dict.
                         print(f"Data received from client: {received_data }")
                         
@@ -304,6 +329,9 @@ class Raspberry_Server( Common_Operations ):
             except Exception as e:
                 print("An error occurred: ", e)
                 traceback.print_exc()
+                # Delete the connection object
+                del self.server_operations.conn
+                
                 continue
 
 # Main function
@@ -315,6 +343,7 @@ if __name__ == "__main__":
         HOST=HOST, 
         PORT=PORT, 
         dev='/dev/ttyUSB0',
-        baud_rate=19200
+        baud_rate=19200,
+        camera_port=8080
     )
         
